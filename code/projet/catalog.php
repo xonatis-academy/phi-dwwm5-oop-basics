6bc9bf7579feb337e73be97e23dbc45bc097e8bc
xonatis-academy <formation@xonatis.com>
2021-02-16 14:49:02 +0100

    Adding catalog page

---
+++ code/projet/catalog.php
@@ -0,0 +1,20 @@
+<?php
+
+// 1. Se connecter a la base de données
+$connection = new PDO('mysql:host=localhost;port=3306;dbname=phi_dwwm5', 'root', '');
+$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+// 2. Récupération des lignes de la table velos
+// 2.1 Envoyer une requete SQL pour tout sélectionner
+$resultat = $connection->query('SELECT * FROM velos');
+// 2.3 Pour chaque ligne du resultat de notre requete
+$velos = [];
+while ($ligne = $resultat->fetch())
+{
+    // Ajouter la ligne dans le tableau $velos
+    array_push($velos, $ligne);
+}
+
+var_dump($velos);
+?>
